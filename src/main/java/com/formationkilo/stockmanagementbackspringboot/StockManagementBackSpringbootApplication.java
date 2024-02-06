e0d1be2f099526b0a1ba698021d7fe469b192198
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-06 09:03:41 +0100

    first commit

---
+++ src/main/java/com/formationkilo/stockmanagementbackspringboot/StockManagementBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.formationkilo.stockmanagementbackspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementBackSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManagementBackSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/formationkilo/stockmanagementbackspringboot/StockManagementBackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formationkilo.stockmanagementbackspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = StockManagementBackSpringbootApplicationTests.class)
+class StockManagementBackSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
